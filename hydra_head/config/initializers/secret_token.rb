73ba666483598d01919bbe1ddb80e34c1c2fdf7b
Jacob Larsen <jac@kb.dk>
2013-01-04 09:29:12 +0100

    added a default hydra-head

---
+++ hydra_head/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraHead::Application.config.secret_token = 'bd3efc1791a1570c2d9cc9ea981f6f42f199e842d21ad0d2d27ac033937caf6334afafd45809bbcadc46b15e43c6dfb03dd21007a9f9de2a6cc9b7a0a7455cc4'
