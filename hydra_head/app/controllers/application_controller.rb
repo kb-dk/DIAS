8c3212d48ec97b5375d0182d583ddb5ee38a21e6
David Grove Jørgensen <dgj@kb.dk>
2013-01-09 10:49:16 +0100

    added multi language support

--- hydra_head/app/controllers/application_controller.rb
+++ hydra_head/app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
   layout 'blacklight'
 
   protect_from_forgery
+
+  # set the locale
+  before_filter :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:change_locale] || params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
 end
