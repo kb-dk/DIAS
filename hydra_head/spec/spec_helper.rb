557e049a8bc8bd0e36ddde37f8a462d73cd666c8
Andreas <andreas.westh@gmail.com>
2013-01-09 22:51:00 +0100

    tilføjet fixtures (indlæsning af xml eksempel). For spec tests

--- hydra_head/spec/spec_helper.rb
+++ hydra_head/spec/spec_helper.rb
@@ -7,6 +7,10 @@ require 'rspec/autorun'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+ 
+ def fixture(file)
+    File.new(File.join(File.dirname(__FILE__), 'fixtures', file))
+  end
 
 RSpec.configure do |config|
   # ## Mock Framework
