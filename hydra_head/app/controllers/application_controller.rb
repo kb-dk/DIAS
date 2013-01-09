b3aba8ec7b93d66d0131fed07608b3fd83ae7396
David Grove Jørgensen <dgj@kb.dk>
2013-01-09 13:48:29 +0100

    fix bug in change language

--- hydra_head/app/controllers/application_controller.rb
+++ hydra_head/app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
  
   def set_locale
-    I18n.locale = params[:change_locale] || params[:locale] || I18n.default_locale
+    I18n.locale =  params[:locale] || I18n.default_locale
   end
 
   def default_url_options(options={})
