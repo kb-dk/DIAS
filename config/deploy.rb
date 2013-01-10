d5a3b36734e8986c2b27350fb671972e615079fb
David Grove Jørgensen <dgj@kb.dk>
2013-01-10 17:00:51 +0100

    added some db:migrate stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :scm, :git # You can set :scm explicitly or Capistrano will make an intellig
 
 role :web, "disdev-01.kb.dk"                          # Your HTTP server, Apache/etc
 role :app, "disdev-01.kb.dk"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "disdev-01.kb.dk", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
@@ -28,3 +28,5 @@ namespace :deploy do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
  end
+
+after "deploy", "deploy:migrate"
