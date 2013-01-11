923803710d61f2403298f72c02b8b9436cd715d2
Jacob Larsen <jac@kb.dk>
2013-01-11 14:09:09 +0100

    added username and pid to model

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(:version => 20130107093646) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "pid"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "guest",                  :default => false
