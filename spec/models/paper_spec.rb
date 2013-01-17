e37255d1622e8dc3286a6582862a5681b1392798
David Grove Jørgensen <dgj@kb.dk>
2013-01-17 16:11:42 +0100

    added file upload

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Paper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "Should have metadata stream named descMetadata" do
+    subject.descMetadata.class.should == OpgaveModsDatastream
+  end 
+
+  it "Should have rightsmetadata stream named rightsMetadat" do
+    subject.rightsMetadata.class.should == Hydra::Datastream::RightsMetadata
+  end 
+
+  it "Should have file data stream named 'content'" do
+    subject.content.content.should == Hydra::Model::FileAsset
+  end 
+
+
+
 end
