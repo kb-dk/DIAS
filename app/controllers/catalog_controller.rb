f5a033f2ce87e93c92fda9210d1c0d2efe58c2fd
Andreas <andreas.westh@gmail.com>
2013-01-21 11:06:30 +0100

    Genre -> Type og korrektsamlet afleverings År -> Afleveringsår

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -56,9 +56,9 @@ class CatalogController < ApplicationController
     #config.add_facet_field 'subject_geo_facet', :label => 'Region'
     #config.add_facet_field 'subject_era_facet', :label => 'Era'
     config.add_facet_field  'studium_facet', :label => 'Studium'
-    config.add_facet_field  'afleveringsaar_facet', :label => 'Afleverings År'
+    config.add_facet_field  'afleveringsaar_facet', :label => 'Afleveringsår'
     config.add_facet_field  'opgavesprog_facet', :label => 'Sprog'
-    config.add_facet_field  'genre_facet', :label => 'Genre'
+    config.add_facet_field  'genre_facet', :label => 'Type'
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
