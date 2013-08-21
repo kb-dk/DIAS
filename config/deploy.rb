19c6feae72f53d631c4d24d4143591715a256ee0
Blimey74 <jatr@kb.dk>
2013-08-21 16:33:06 +0200

    Copy the fedora and solr config files to the jetty installation

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,9 +32,16 @@ namespace :deploy do
   # custom task to copy dias.yml to current deploy
    task :config_dias, :roles => :app, :except => { :no_release => true}  do
      run "cp /home/dias/dias.yml #{File.join(current_path,'config')}"
-     run "pwd"
-     run "cd #{current_path}; bundle exec rake jetty:config"
    end;
+
+   task :config_jetty, :roles => :app, :except => { :no_release => true}  do
+     run "cd #{current_path}; cp ./solr_conf/conf/schema.xml ../../jetty/solr/development-core/conf"
+     run "cd #{current_path}; cp ./solr_conf/conf/schema.xml ../../jetty/solr/test-core/conf"
+     run "cd #{current_path}; cp ./solr_conf/conf/solrconfig.xml ../../jetty/solr/development-core/conf"
+     run "cd #{current_path}; cp ./solr_conf/conf/solrconfig.xml ../../jetty/solr/test-core/conf"
+     run "cd #{current_path}; cp ./fedora_conf/conf/development/fedora.cfg ../../jetty/fedora/default/server/config"
+     run "cd #{current_path}; cp ./fedora_conf/conf/test/fedora.cfg ../../jetty/fedora/test/server/config"
+   end
  
    # task to seed db
    task :seed do
@@ -45,5 +52,6 @@ namespace :deploy do
 after "deploy", "deploy:migrate"
 after "deploy", "deploy:seed"
 after "deploy", "deploy:config_dias"
+after "deploy", "deploy:config_jetty"
 after "deploy:restart", "deploy:cleanup"
 
